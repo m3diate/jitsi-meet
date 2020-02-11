e4bd604374dead18be25b032da0a08e8e97bb1e9
Christian <christian@nyu.edu>
2020-02-11 18:03:57 -0500

    deep links

--- react/features/deep-linking/components/DeepLinkingMobilePage.web.js
+++ react/features/deep-linking/components/DeepLinkingMobilePage.web.js
@@ -116,14 +116,14 @@ class DeepLinkingMobilePage extends Component<Props> {
                 <div className = 'header'>
                     <img
                         className = 'logo'
-                        src = 'images/logo-deep-linking.png' />
+                        src = 'images/patchlogo.png' />
                 </div>
                 <div className = { `${_SNS}__body` }>
                     {
                         SHOW_DEEP_LINKING_IMAGE
                             ? <img
                                 className = 'image'
-                                src = 'images/deep-linking-image.png' />
+                                src = 'images/patchlogo.png' />
                             : null
                     }
                     <p className = { `${_SNS}__text` }>
